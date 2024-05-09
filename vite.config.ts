6885e1a2077e526ab6c381e8e88bc90a5921f49b
eythorsson <steinar@wise.no>
2024-05-09 11:44:48 +0200

    Project setup

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    plugins: [
+        dts({
+            include: ['src'],
+            exclude: "**/*.test.ts"
+        })
+    ],
+    build: {
+        manifest: true,
+        minify: true,
+        reportCompressedSize: true,
+        lib: {
+            entry: "./src/index.ts",
+            formats: ['es'],
+            name: "common-utils",
+            fileName: (format) => `index.${format}.js`,
+        },
+    }
+})
