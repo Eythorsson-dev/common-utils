cbd738aa84a0c4c857086c2e989cf88943bfa927
eythorsson <steinar@wise.no>
2024-05-14 21:25:55 +0200

    Export ActionableItem

--- src/linkedList/index.ts
+++ src/linkedList/index.ts
@@ -4,6 +4,11 @@ export * from "./getNextItem"
 export * from "./getNextSiblings"
 export * from "./getNextOrChildById"
 export * from "./upsertAndReturnRoot"
-export { type Item, type ItemData, ItemElement} from "./item"
+export {
+    type Item,
+    type ItemData,
+    type ActionableItem,
+    ItemElement
+} from "./item"
 export * from "./sortList"
 export * from "./validateList"
