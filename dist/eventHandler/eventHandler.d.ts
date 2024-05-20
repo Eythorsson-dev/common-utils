5c770e4799cfcc29200dc60a76a2712f0cbb7e76
eythorsson <steinar@wise.no>
2024-05-20 15:51:15 +0200

    Build

--- dist/eventHandler/eventHandler.d.ts
+++ dist/eventHandler/eventHandler.d.ts
@@ -9,7 +9,7 @@ export interface EventHandlerAPI<EventMap extends {
     [key in keyof EventMap]: Omit<Event<EventMap, key, TBaseEvent>, "preventDefault">;
 }, TBaseEvent extends EventFunctions = BaseEvent> {
     On<Key extends keyof EventMap>(type: Key, handler: (event: Event<EventMap, Key, TBaseEvent>) => void): EventListenerId;
-    Once<Key extends keyof EventMap>(type: Key, handler: (event: Event<EventMap, Key, TBaseEvent>) => void): EventListenerId;
+    Once<Key extends keyof EventMap>(type: Key, handler: (event: Event<EventMap, Key, TBaseEvent>) => void, condition: (event: Event<EventMap, Key, TBaseEvent>) => boolean): EventListenerId;
     Off<Key extends keyof EventMap>(type: Key, listener: EventListenerId): void;
     Emit<Key extends keyof EventMap>(type: Key, event: Omit<Event<EventMap, Key, TBaseEvent>, "preventDefault">): {
         preventDefault: boolean;
